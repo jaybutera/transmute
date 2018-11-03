97674c3caef11cc5a27c6ca62f7753d396f306c5
Jay Butera <buterajay@gmail.com>
2018-11-03 20:38:34 +0100

    A little cleaner

--- test/transmute.js
+++ test/transmute.js
@@ -1,24 +1,23 @@
 const Transmute = artifacts.require('Transmute')
 const ET = artifacts.require('ExampleToken')
 
-contract('Test transmute', accounts => {
+contract('Test transmute', ([owner, user]) => {
    let transmute
 
    beforeEach( async () => {
-      token     = await ET.new(1000,                  { from: accounts[0] })
-      transmute = await Transmute.new( token.address, { from: accounts[0] })
+      token     = await ET.new(1000,                  { from: owner })
+      transmute = await Transmute.new( token.address, { from: owner })
 
       // Send eth to contract
-      web3.eth.sendTransaction({ from: accounts[0], to: transmute.address, value: 1000 })
+      web3.eth.sendTransaction({ from: owner, to: transmute.address, value: 1000 })
    })
 
    it('Should transmute', async () => {
-      const user = accounts[1]
-      await token.transfer(user, 500, { from: accounts[0] })
+      await token.transfer(user, 500,             { from: owner })
       await token.approve(transmute.address, 500, { from: user })
 
       const pre_bal = (await web3.eth.getBalance(user)).toNumber()
-      await transmute.transformFrom(user, 500, { from: accounts[0] })
+      await transmute.transformFrom(user, 500,    { from: owner })
 
       const bal = (await web3.eth.getBalance(user)).toNumber()
 
