a124f3102af800d3c54cc4cb11a81da17f20d2aa
Jay Butera <buterajay@gmail.com>
2018-11-04 11:13:04 +0100

    Token is mintable

--- contracts/ExampleToken.sol
+++ contracts/ExampleToken.sol
@@ -1,8 +1,9 @@
 pragma solidity ^0.4.23;
 
 import 'openzeppelin-solidity/contracts/token/ERC20/ERC20Burnable.sol';
+import 'openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol';
 
-contract ExampleToken is ERC20Burnable {
+contract ExampleToken is ERC20Burnable, ERC20Mintable {
     constructor (uint256 _supply) {
         _mint(msg.sender, _supply);
     }
